d3c6af9e168dca217c14736d53e4de44d1eaea64
mtc2013 <mtc2013opensource@gmail.com>
2017-07-07 17:16:15 -0400

    More mucking about

--- repeated_cross_validation.R
+++ repeated_cross_validation.R
@@ -1,16 +1,14 @@
 data = read.csv("data.csv")
 nonzerodata = subset(data, week1 > 0 | week2 > 0 | week3 > 0)
-pr = princomp(nonzerodata[,1:3], cor=TRUE, scale=TRUE)
-nonzerodata$comp1 = pr$scores[,1]
 
 library(caTools)
 set.seed(1000)
 
 library(caret)
 numFolds = 5
-nonzerodata$folds <- createFolds(factor(nonzerodata$premium), k=numFolds, list=FALSE)
 
 library("ROCR")
+libray(DMwR)
 aucs <- c()
 for(rep in 1:1000){
   nonzerodata$folds <- createFolds(factor(nonzerodata$premium), k=numFolds, list=FALSE)
@@ -18,12 +16,17 @@ for(rep in 1:1000){
   for(i in 1:numFolds){
     train = subset(nonzerodata, folds != i)
     test  = subset(nonzerodata, folds == i)
-
+    
+    #train$premium <- factor(ifelse(train$premium == 1,"rare","common")) 
+    #test$premium <- factor(ifelse(test$premium == 1, "rare", "common"))
+    #train <- SMOTE(premium ~ ., train, perc.over = 500,perc.under=150)
+    
+    pComp = prcomp(~ week1 + week2 + week3, train, scale = TRUE)
+    train$comp1 = pComp$x[,1]
+    test$comp1 = predict(pComp,newdata=test)[,1]
+    
     model = glm(premium ~ comp1 ,data=train,family="binomial")
-    probTrain=predict(model,type=c("response"))    
-    predTrain <- prediction(probTrain, train$premium)   
-    perfTrain <- performance(predTrain, "auc")
-
+    
     probTest = predict(model, type="response", newdata=test)
     predTest <- prediction(probTest, test$premium)   
     perfTest <- performance(predTest, "auc")
@@ -31,5 +34,5 @@ for(rep in 1:1000){
   }
   #print(aucs[((rep-1)*numFolds + 1) : ((rep-1)*numFolds + numFolds)])
 }
-sd(aucs) #.1797646
-mean(aucs) #.6628563
+sd(aucs) 
+mean(aucs)
